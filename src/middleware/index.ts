2d7b4f82ca703c6b27fa3b6339b870694c45457f
mecamon <arq.camejia@gmail.com>
2024-02-25 13:23:09 -0400

    fix on middleware comments

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,11 +1,11 @@
-// import { defineMiddleware } from "astro:middleware";
+import { defineMiddleware } from "astro:middleware";
 
-// // `context` and `next` are automatically typed
-// export const onRequest = defineMiddleware(async (context, next) => {
-//   console.log("HELLO WORLD!");
-//   await Promise.resolve();
-//   if (context.url.pathname === "/about") {
-//     return context.redirect("/");
-//   }
-//   return next();
-// });
+// `context` and `next` are automatically typed
+export const onRequest = defineMiddleware(async (context, next) => {
+  // console.log("HELLO WORLD!");
+  // await Promise.resolve();
+  // if (context.url.pathname === "/about") {
+  //   return context.redirect("/");
+  // }
+  return next();
+});
