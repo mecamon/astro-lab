fd1ddb214911abe2c6676304bebfd2f48bf1ca47
mecamon <arq.camejia@gmail.com>
2024-02-25 13:26:22 -0400

    fix on about routes

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -2,10 +2,10 @@ import { defineMiddleware } from "astro:middleware";
 
 // `context` and `next` are automatically typed
 export const onRequest = defineMiddleware(async (context, next) => {
-  // console.log("HELLO WORLD!");
-  // await Promise.resolve();
-  // if (context.url.pathname === "/about") {
-  //   return context.redirect("/");
-  // }
+  console.log("HELLO WORLD!");
+  await Promise.resolve();
+  if (context.url.pathname === "/astro-lab/about") {
+    return context.redirect("/");
+  }
   return next();
 });
